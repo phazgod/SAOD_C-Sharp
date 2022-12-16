85a245d96029b716ae0841ebb285c2d88d1e4d06
Georgiy <zhorik.eresko@mail.ru>
2022-12-16 21:44:14 +0300

    Сдача List

---
+++ SAOD_List/MyList_SAOD/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyList_SAOD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MyList<int> list = new MyList<int>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 15; i++)
+            {
+                list.Append(r.Next(0, 51));
+            }
+
+            textBox8.Text = list.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Append(Convert.ToInt32(textBox1.Text));
+
+            textBox8.Text = list.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Prepend(Convert.ToInt32(textBox2.Text));
+
+            textBox8.Text = list.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                list.Insert(Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox9.Text));
+
+                textBox8.Text = list.ToString();
+            }
+
+            catch
+            {
+                MessageBox.Show("нельзя добавить элемент с данным индексом.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = list.Find(Convert.ToInt32(textBox4.Text)).ToString();
+                textBox10.Text = str;
+            }
+
+            catch
+            {
+                MessageBox.Show("Нет элемента с таким значением.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int zn = list.At(Convert.ToInt32(textBox5.Text));
+                textBox11.Text = zn.ToString();
+            }
+
+            catch
+            {
+                MessageBox.Show("Нет элементов с таким индексом.");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox12.Text = list.Remove(Convert.ToInt32(textBox6.Text)).ToString();
+            textBox8.Text = list.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox13.Text = list.RemoveAt(Convert.ToInt32(textBox7.Text)).ToString();
+            textBox8.Text = list.ToString();
+        }
+    }
+}
---
+++ SAOD_List/MyList_SAOD/MyList.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList_SAOD
+{
+    public class MyList<T>  where T: IEquatable<T> //могу сравнивать элементы типа <T>
+    {
+        ListNode<T> first; //первый элемент
+        ListNode<T> last; //последний элемент
+        int count = 0; //кол-во элементов
+
+
+        public void Append(T val) //добавление элемента в конец. (Next, Prev, Value)
+        {
+            if (first == null)
+            {
+                first = new ListNode<T>(null, null, val); //создает новый элемент, записывается в переменную первой
+                last = first; //из-за того, что один элемент, он первый и последний
+            }
+
+            else
+            {
+                ListNode<T> tmp = new ListNode<T>(null, last, val); //создает новый элемент
+                last.Next = tmp; //меняю ссылку пред на след
+                last = tmp; //последний эл становится тем, который добавил
+            }
+
+            count++;
+        }
+
+        public void Prepend(T val) //Добавление в начало.
+        {
+            if (first == null)
+            {
+                first = new ListNode<T>(null, null, val);
+                last = first;
+            }
+            else
+            {
+                ListNode<T> tmp = new ListNode<T>(first, null, val);
+                first.Prevs = tmp; //меняю ссылку на пред элемент
+                first = tmp; //добавленный эл становится первым
+            }
+            count++;
+
+        }
+
+        public void Insert(T val, int index) // Добавление в середину.
+        {
+            int col = 0; //счетчик
+            ListNode<T> tmp = first; //сохранение 1го элемента в tmp
+
+            while (col != index) //счетчик = индекс
+            {
+                if (tmp == null) //проверка (если нет 1го элемента, то не можешь)
+                {
+                    throw new Exception();
+                }
+
+               tmp = tmp.Next; //след элемент
+                col++;
+            }
+
+            ListNode<T> tmp_p = new ListNode<T>(tmp, tmp.Prevs, val); //когда нашел куда добавлять
+            tmp.Prevs.Next = tmp_p; //меняю ссылку пред эл на след
+            tmp.Prevs = tmp_p; //пред эл меняю на добавленный
+
+            count++;
+        }
+
+        public int Find(T val) // Возвращать позицию элемента. Индекс элемента с этим значением.
+        {
+            ListNode<T> tmp = first;
+            int col = 0;
+
+            while (!tmp.Value.Equals(val)) //equals позволяет сравнивать значения типа <T>
+            {
+                if (tmp.Next == null)
+                {
+                    throw new Exception();
+                }
+                tmp = tmp.Next;
+                col++;
+            }
+            return col; //возрат индекса элемента
+        }
+
+        public T At(int index) // Значение элемента. Если на этой позиции нет, то ошибка.
+        {
+            if(index < 0 || index > count-1) // || <--- или
+            {
+                throw new Exception();
+            }
+
+            int col = 0;
+            ListNode<T> tmp = first; //tmp - значение
+
+            while (col != index)
+            {
+                tmp = tmp.Next; //сохраняю значение
+                col++;
+            }
+
+            return tmp.Value;
+        }
+
+        public bool Remove(T val) // По значению.
+        {
+            ListNode<T> tmp = first;
+
+            while (!tmp.Value.Equals(val)) // ! <--- пока значение tmp != введенному значению
+            {
+                if (tmp.Next == null)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    tmp = tmp.Next;
+                }
+            }
+            if (tmp.Next == null && tmp.Prevs == null) //&& <-- and
+            {
+                first = last = null; // 1 = 2 и удаляем
+            }
+            else if (tmp.Prevs == null)
+            {
+                first = tmp.Next; //меняем ссылку 1эл
+                tmp.Next.Prevs = null; //меняем ссылку 3эл
+            }
+            else if (tmp.Next == null)
+            {
+                last = tmp.Prevs;
+                tmp.Prevs.Next = null; //у след эл нет пред
+            }
+            else
+            {
+                tmp.Prevs.Next = tmp.Next;
+                tmp.Next.Prevs = tmp.Prevs;
+            }
+            count--;
+            return true;
+        }
+
+        public bool RemoveAt(int index) // По индексу. тоже самое Remove, только по индексу
+        {
+            int col = 0;
+            ListNode<T> tmp = first;
+
+            while (col != index)
+            {
+                if (tmp.Next == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    tmp = tmp.Next;
+                    col++;
+                }
+            }
+
+
+            if (tmp.Next == null && tmp.Prevs == null)
+            {
+                first = last = null;
+            }
+
+            else if (tmp.Prevs == null)
+            {
+                tmp.Next.Prevs = null;
+            }
+
+            else if (tmp.Next == null)
+            {
+                tmp.Prevs.Next = null;
+            }
+
+            else
+            {
+                tmp.Prevs.Next = tmp.Next;
+                tmp.Next.Prevs = tmp.Prevs;
+            }
+
+            count--;
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            ListNode<T> tmp_2 = first;
+            str += first.Value + "\r\n";
+
+            while (tmp_2.Next != null)
+            {
+                tmp_2 = tmp_2.Next;
+                str += tmp_2.Value + "\r\n";
+            }
+
+            return str;
+        }
+    }
+}
